f34b76e92386533f56bfb9ba43673b9867bde7f3
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-09 11:57:46 +0530

    feat: Add user registration route and integrate with server.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./src/config/db.js";
+import userRoutes from "./src/routes/userRoutes.js"
 
 const app = express();
 
@@ -8,9 +9,15 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
+//Middleware
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// Routes
+app.use("/api/user", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
