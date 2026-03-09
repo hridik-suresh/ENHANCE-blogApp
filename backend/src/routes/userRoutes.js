e486ba6a759d9788e07aac39a496c6f9c64ae573
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-09 14:03:43 +0530

    feat: Add user login route to userRoutes.

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { register } from "../controllers/userController.js";
+import { register, login } from "../controllers/userController.js";
 
 const router = express.Router();
 
 // User Registration Route
 // POST /api/user/register
 router.post("/register", register);
+
+// User Login Route
+// POST /api/user/login
+router.post("/login", login);
 
 export default router;
