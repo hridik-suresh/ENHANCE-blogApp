ccb97630de82202ad8181d35152a1ee4e6252576
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-09 11:39:24 +0530

    feat: Controller function to register the user is created.

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,60 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/User.js";
+
+//@desc    Register a new user-----------------------------------------------------------------------------
+//@route   POST /api/user/register
+//@access  Public
+export const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!firstName || !lastName || !email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: "Password must be at least 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Account Created Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to register",
+    });
+  }
+};
