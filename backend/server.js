b0731c6e9ffa1bc973d671326fc6539e66fa0fc6
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-09 08:22:35 +0530

    feat: Connected to MongoDB Atlas successfully.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import connectDB from "./src/config/db.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
