8cd3baab20b7e57dd509eb18446629b9f0c4b72e
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-10 17:08:50 +0530

    feat: ThemeProvider component is created.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { Toaster } from "sonner";
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import ThemeProvider from "./components/ThemeProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
       <Toaster />
     </Provider>
   </StrictMode>,
