add43ee438039a28f760cd4215fc606da06ef2c8
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-10 16:54:35 +0530

    feat: Login page is created.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,124 @@
+import { useState } from "react";
+import { Input } from "../components/ui/input";
+import { Button } from "../components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
+import { Label } from "../components/ui/label";
+import { Eye, EyeOff } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { toast } from "sonner";
+import { useDispatch } from "react-redux";
+import { setUser } from "../redux/authSlice";
+import auth from "../assets/auth.jpg";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(input);
+
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_BACKEND_URL}/api/user/login`,
+        input,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        },
+      );
+      if (response.data.success) {
+        navigate("/");
+        dispatch(setUser(response.data.user));
+        toast.success(response.data.message);
+      }
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <div className="flex items-center h-screen md:pt-14 md:h-190 ">
+      <div className="hidden lg:flex flex-1 items-center justify-center bg-muted">
+        <img src={auth} alt="" className="h-full w-full object-cover" />
+      </div>
+      <div className="flex justify-center items-center flex-1 px-4 md:px-0">
+        <Card className="w-full max-w-md p-6 shadow-lg rounded-2xl dark:bg-gray-800 dark:border-gray-600">
+          <CardHeader>
+            <CardTitle className="text-center text-xl font-semibold">
+              Login into your account
+            </CardTitle>
+            <p className="text-gray-600 dark:text-gray-300 mt-2 text-sm font-serif text-center">
+              Enter your details below to login your account
+            </p>
+          </CardHeader>
+          <CardContent>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <div>
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  placeholder="Email Address"
+                  name="email"
+                  value={input.email}
+                  onChange={handleChange}
+                  required
+                  className="dark:border-gray-600 dark:bg-gray-900"
+                />
+              </div>
+
+              <div className="relative">
+                <Label>Password</Label>
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter Your Password"
+                  name="password"
+                  value={input.password}
+                  onChange={handleChange}
+                  required
+                  className="dark:border-gray-600 dark:bg-gray-900"
+                />
+                <button
+                  type="button"
+                  className="absolute right-3 top-6 text-gray-500"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                </button>
+              </div>
+
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <p className="text-center text-gray-600 dark:text-gray-300">
+                Don't have an account?{" "}
+                <Link to={"/signup"}>
+                  <span className="underline cursor-pointer hover:text-gray-800">
+                    Sign up
+                  </span>
+                </Link>
+              </p>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
