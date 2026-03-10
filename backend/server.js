51b8f690c82764a3b219a761cc816d170db3dbb6
hridik-suresh <iamhridiksuresh@gmail.com>
2026-03-10 15:40:16 +0530

    feat: Signup page is created. New user will be created in the database.

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import connectDB from "./src/config/db.js";
-import userRoutes from "./src/routes/userRoutes.js"
+import userRoutes from "./src/routes/userRoutes.js";
 
 const app = express();
 
@@ -10,6 +11,13 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 //Middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Button } from "../components/ui/button";
 
 const Home = () => {
   return (
-    <div>
+    <div className="pt-20">
       <Hero />
     </div>
   );
